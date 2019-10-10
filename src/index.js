73b900f45740b1d28a9ea41161def22cfd4110c5
Dmitrukk <47752464+Dmitrukk@users.noreply.github.com>
2019-10-10 23:17:30 +0300

    Implementation of decode function

--- src/index.js
+++ src/index.js
@@ -38,9 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let decoderStr = "";
+    const lettersArray = expr.match(/.{1,10}/g);
+    lettersArray.forEach((element) => {
+        let codeStr = "";
+        for (let i=0; i<element.length; i+=2) {
+        let item =  element.substr(i, 2);
+        if (item === '11') {
+            codeStr += '-';
+        }
+        if (item === '10') {
+            codeStr += '.';
+        }
+        if (item === '**') {
+            i = element.lenght;
+            codeStr += ' ';
+        }
+    }
+        for (let key in MORSE_TABLE) {
+            if (key === codeStr) {
+                decoderStr += MORSE_TABLE[key];
+            }
+            
+        }
+        if (codeStr === ' ') {
+            //console.log(' ');
+            decoderStr +=' ';
+        }
+      });
+      return decoderStr;
 }
 
+
 module.exports = {
-    decode
+   decode
 }
